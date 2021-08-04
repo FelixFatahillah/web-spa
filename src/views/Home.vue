d686c736ac229f6083d7caf888781f921582bd21
FelixFatahillah <felix.fatahillah22@gmail.com>
2021-08-05 00:38:24 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <Header />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+import Header from '@/components/Header.vue'
+export default {
+  name: 'Home',
+  components: {
+    Navbar,
+    Header
+
+    
+  }
+}
+</script>
